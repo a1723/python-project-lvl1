35369766677aafed7fbe228c748ee3cb113935d1
Aleksandr <Sashkakonowawka4@gmail.com>
2021-05-06 18:06:26 +0300

    fix

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -8,12 +8,12 @@ def get_mutable_element_index(progression):
     return mutable_element_index
 
 def get_mutable_element_value(progression, mutable_element_index):
-    #print(progression[mutable_element_index])
-    mutable_element_value = progression[mutable_element_index]
+    mutable_element_value = str(progression[mutable_element_index])
     return mutable_element_value
 
 def get_changed_progression(progression, mutable_element_value):
     changed_progression = " ".join(map(str, progression))
+    print(changed_progression)
     changed_progression = changed_progression.replace(str(mutable_element_value), '..')
     return changed_progression
 
@@ -28,6 +28,6 @@ def main():
         mutable_element_value = get_mutable_element_value(progression, mutable_element_index)
         changed_progression = get_changed_progression(progression, mutable_element_value)
         answer = get_user_answer(changed_progression)
-        correct_answer = str(mutable_element_value)
+        correct_answer = mutable_element_value
         count_correct_answers = check_answer(answer, correct_answer, count_correct_answers)
     get_congratulations()
